4fa40ea68f37040893eb8293ed06e23259cc3c91
Marcelo Herzog <60850782+marceloherzog@users.noreply.github.com>
2020-07-01 20:14:33 -0300

    Update ReScan_HBA.ps1

--- ReScan_HBA.ps1
+++ ReScan_HBA.ps1
@@ -1,5 +1,5 @@
-﻿Connect-VIServer vim-vm01-vcsa06.oss.timbrasil.com.br
-$cluster = Get-Cluster -name FLA04_SILO_IMS_CORE*
+﻿Connect-VIServer VCENTER_NAME
+$cluster = Get-Cluster -name CLUSTER_NAME
 $hostlist = Get-VMHost -location $cluster | Sort-Object
 foreach ($myhost in $hostlist) {
  Get-VMHostStorage -VMHost $myhost -RescanAllHba -RescanVmfs
