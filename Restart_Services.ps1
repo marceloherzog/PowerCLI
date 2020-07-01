bbb2308c23d906e1deaebced15227a29e4cd512f
Marcelo Herzog <60850782+marceloherzog@users.noreply.github.com>
2020-07-01 20:18:23 -0300

    Update Restart_Services.ps1

--- Restart_Services.ps1
+++ Restart_Services.ps1
@@ -1,18 +1,17 @@
-﻿Connect-VIServer vim-vm01-vcsa10.oss.timbrasil.com.br
-$mycluster = Get-Cluster BLM01_AAA
+﻿Connect-VIServer VCENTER_SERVER
+$mycluster = Get-Cluster CLUSTER
 $hostlist = Get-VMHost -Location $mycluster | Sort-Object
 foreach ($myhost in $hostlist) {
 
-#$vpxa = Get-VMHost -Name $myhost | Get-VMHostService | where {$_.key -eq 'vpxa'}
-#$fdm = Get-VMHost -Name $myhost | Get-VMHostService | where {$_.key -eq 'vmware-fdm'}
-#$esxi_shell = Get-VMHost -Name $myhost | Get-VMHostService | where {$_.key -eq 'TSM'}
+$vpxa = Get-VMHost -Name $myhost | Get-VMHostService | where {$_.key -eq 'vpxa'}
+$fdm = Get-VMHost -Name $myhost | Get-VMHostService | where {$_.key -eq 'vmware-fdm'}
+$esxi_shell = Get-VMHost -Name $myhost | Get-VMHostService | where {$_.key -eq 'TSM'}
 $ssh = Get-VMHost -Name $myhost | Get-VMHostService | where {$_.key -eq 'TSM-SSH'}
 
-#Restart-VMHostService -HostService $vpxa -Confirm:$false
-#Restart-VMHostService -HostService $fdm -Confirm:$false
-#Restart-VMHostService -HostService $esxi_shell -Confirm:$false
-#Stop-VMHostService -HostService $esxi_shell -Confirm:$false
-Stop-VMHostService -HostService $ssh -Confirm:$false
+Restart-VMHostService -HostService $vpxa -Confirm:$false
+Restart-VMHostService -HostService $fdm -Confirm:$false
+Restart-VMHostService -HostService $esxi_shell -Confirm:$false
+Restart-VMHostService -HostService $ssh -Confirm:$false
 
 }
 Disconnect-VIServer -Confirm:$false
