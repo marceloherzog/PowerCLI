8947fc6a9855cc6db52432351201e649530c5222
Marcelo Herzog <60850782+marceloherzog@users.noreply.github.com>
2020-07-01 20:15:45 -0300

    Update Remove_Snapshot.ps1

--- Remove_Snapshot.ps1
+++ Remove_Snapshot.ps1
@@ -1,14 +1,12 @@
 ﻿if ($vccred) { Remove-Variable vccred }
 if ($global:DefaultVIServers) { Disconnect-VIServer -Server * -Confirm:$false }
 
-$vccred = Get-Credential -Credential F8073716
+$vccred = Get-Credential -Credential LOGIN
 if (-not $vccred) { Return "Invalid Credential" }
 
-$ClusterWildCard = "*STPDRA"
+$ClusterWildCard = "*CLUSTER_NAME"
 
-$vclist = "nfvvm01-vcn-01.timbrasil.nfvi","nfvvm01-vcn-02.timbrasil.nfvi","nfvvm01-vcn-03.timbrasil.nfvi","nfvvm01-vcn-05.timbrasil.nfvi",
-"vim-vm01-vcsa01","vim-vm01-vcsa02","vim-vm01-vcsa03","vim-vm01-vcsa04","vim-vm01-vcsa05","vim-vm01-vcsa06","vim-vm01-vcsa07",
-"vim-vm01-vcsa08","vim-vm01-vcsa09","vim-vm01-vcsa10"
+$vclist = "VCENTER_LIST"
 
 foreach ($myvc in $vclist) {
 
