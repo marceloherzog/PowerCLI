0800784a0418916f8d5a98fc5f9fd74cc289eee0
Marcelo Herzog <60850782+marceloherzog@users.noreply.github.com>
2020-07-01 20:16:39 -0300

    Update Replace_NTP_Servers.ps1

--- Replace_NTP_Servers.ps1
+++ Replace_NTP_Servers.ps1
@@ -1,8 +1,8 @@
-﻿Connect-VIServer vim-vm01-vcsa06.oss.timbrasil.com.br
-$mycluster = Get-Cluster FLA01_SILO_IMS_SBC_01
+﻿Connect-VIServer VCENTER_NAME
+$mycluster = CLUSTER_NAME
 $hostlist = Get-VMHost -Location $mycluster | Sort-Object
 # NTP Servers Array
- $NTPServers = "10.216.96.174","10.192.12.200","10.221.6.229","10.223.49.160"
+ $NTPServers = ""
 foreach ($myhost in $hostlist) {
 
 $ntpsrvs = $myhost.ExtensionData.Config.DateTimeInfo.NtpConfig.Server
